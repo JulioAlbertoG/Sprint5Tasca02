a903a916197dbb36550cbbbef0a61cb07b6e0012
Julio <91191266+JulioAlbertoG@users.noreply.github.com>
2022-11-24 19:21:47 +0100

    Delete S05T02N01FASE3GonzalezJulio directory

--- S05T02N01FASE3GonzalezJulio/src/main/java/cat/itacademy/barcelonactiva/gonzalez/julio/s05/t02/n01/f3/security/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package cat.itacademy.barcelonactiva.gonzalez.julio.s05.t02.n01.f3.security;
-
-public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 70000;
-    public static final String JWT_SECRET = "secret";
-}
